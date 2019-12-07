857ce52844e7178cf0a57e03cc6c775035b3486f
Marcela <marceferrazb@gmail.com>
2019-12-06 21:34:12 -0300

    creacion entidades en proceso y main con posibles metodos(sin implementar)

---
+++ CanalEncubiertoObl/src/canalencubiertoobl/CanalEncubiertoOblMain.java
@@ -0,0 +1,47 @@
+package canalencubiertoobl;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CanalEncubiertoOblMain {
+
+    public static void main(String[] args) {
+    }
+    
+    public CanalEncubiertoOblMain(){
+    }
+    
+    public static void ejecutarPrograma()throws IOException {
+    
+    }
+    
+    public static void obtenerSecuenciaChar(BufferedReader sec){
+        
+    }
+    
+    public static char[] leerBytes(File archivo, InputStream inputStreamArchivo) throws IOException{
+        return null;
+    }
+    
+    public static void crearInputStream(){
+    
+    }
+    
+    public static BufferedReader leerNombreArchivo(){
+        return null;
+    }
+    
+//    public static void creatSujeto(String nombre,SecurityLevel securityLevel ){
+
+//}
+    
+    public static boolean existeSujeto(String nombre){
+        return false;
+    }
+    
+    public void obtenerNombreArchivo(){
+    
+    }
+}
